e90a57c81b9470013213d8110a591cc4dddbec03
Nach998 <al036289@iccullera.com>
2025-11-14 12:24:27 +0100

    Add files via upload

---
+++ transpàs.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main(){
+    int any;
+    printf("Introdueix l'any: ");
+    sanf("%d", &any);
+    if(any%4==)
+        if(any%100==0)
+            if(any%400===)
+                printf("Sí q és de transpàs·\n");
+            else printf("No és de transpàs\n");
+        else printf("Sí q és de transpàs·\n");
+    else printf("No és de transpàs\n");
+    return 0;
+}
